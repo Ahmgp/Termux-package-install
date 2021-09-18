1d06f4f0a7032d0ec20eef45949aad9dd3339715
Ahmgp <80210537+Ahmgp@users.noreply.github.com>
2021-09-18 04:34:32 +0200

    Update termux.setup.pkg.py

--- termux.setup.pkg.py
+++ termux.setup.pkg.py
@@ -1,25 +1,25 @@
 Import subprocess
 
 
-Subprocess.call("pkg update ", shell=True)
+subprocess.call("pkg update ", shell=True)
 
-Subprocess.call("pkg upgrade", shell=True)
+subprocess.call("pkg upgrade", shell=True)
 
-Subprocess.call("pkg install unstable-repo",shell=True)
+subprocess.call("pkg install unstable-repo",shell=True)
 
-Subprocess.call("pkg install root-repo",shell=True) 
+subprocess.call("pkg install root-repo",shell=True) 
 
-Subprocess.call("pkg install x11-repo",shell=True)
+subprocess.call("pkg install x11-repo",shell=True)
 
-Subprocess.call("pkg install git", shell=True) 
+subprocess.call("pkg install git", shell=True) 
 
-Subprocess.call("pkg install python", shell=True)
+subprocess.call("pkg install python", shell=True)
 
-Subprocess.call("pkg install python2", shell=True)
+subprocess.call("pkg install python2", shell=True)
 
-Subprocess.call("pkg install python3", shell=True)
+subprocess.call("pkg install python3", shell=True)
 
-Subprocess.call("pkg install wget",shell=True)
+subprocess.call("pkg install wget",shell=True)
 
 
-Subprocess.call("pkg install ruby", shell=True) 
+subprocess.call("pkg install ruby", shell=True) 
